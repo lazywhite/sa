195130263a5b05edc010f6f83c0fd95fd1957bb0
lazywhite <346816483@qq.com>
2015-06-14 18:06:08 +0800

    add design pattern submodule

---
+++ lang/java/LearnJava.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.String;
+import java.security.*;
+//import ./Bicycle.*;
+
+public class LearnJava{
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.print("world\n");
+        byte fooByte = 100;
+        short fooShort = 10000;
+        int fooInt = 1;
+        long fooLong = 100000L;
+        float fooFloat = 234.5f;
+        double fooDouble = 12.44;
+        boolean fooBoolean = true;
+        char fooChar = 'A';
+        final int HOURS_I_WORK_PER_WEEK = 900;
+/*        String fooString = "my first string';
+        System.out.println(fooString);  */
+        int [] intArray = new int[10];
+        int [] y = {9000,800,110};
+ //       bicycleObj = new Bicycle(10,20,30);
+  //      System.out.println(bicycleObj.gear); 
+    }
+}
